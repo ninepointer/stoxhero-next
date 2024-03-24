ec7ee50ac36867ac8e0c58b9f7ac5e7da44630a2
Prateek <prateek@Prateeks-MacBook-Pro-2.local>
2024-03-25 05:01:58 +0530

    jai ganesh

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Work_Sans, Lusitana } from 'next/font/google';
+ 
+export const WorkSans = Work_Sans({ subsets: ['latin'], display: 'swap' });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
